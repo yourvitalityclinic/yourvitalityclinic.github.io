0e02c33aabc907233bfb58c38196d4d968fad3f8
Jan Dziedziak <jas.dzied@gmail.com>
2025-01-29 00:22:22 +0000

    centered results page

--- src/screens/Results.tsx
+++ src/screens/Results.tsx
@@ -11,7 +11,13 @@ interface ResultsScreenProps {
 
 function ResultsScreen(props: ResultsScreenProps) {
   return (
-    <>
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      width: "100vw",
+    }}>
       <div className='title-div'>
         <h1><b>Your results</b></h1>
       </div>
@@ -22,8 +28,12 @@ function ResultsScreen(props: ResultsScreenProps) {
           answers={props.answers}
         />
       </div>
-      <h2><b>Develop continuously</b></h2>
-      <p>Register today and have your results delivered straight to your email.<br />You'll also gain access to the Vitaltiy Clinic newsletter, packed with valuable tips, exciting projects, and practical exercises to keep you motivated and help you work toward your best life.<br />But that's not all! As a subscriber, you'll receive an exclusive offer to book your first 60-minute psychological vitality consultation with me at 50% off the regular price. This session will help you identify your goals and overcome the barriers holding you back from a more fulfilling life.<br />And don't worry- you can unsubscribe at any time.</p>
+      <div style={{
+        width: "70vw",
+      }}>
+        <h2><b>Develop continuously</b></h2>
+        <p>Register today and have your results delivered straight to your email.<br />You'll also gain access to the Vitaltiy Clinic newsletter, packed with valuable tips, exciting projects, and practical exercises to keep you motivated and help you work toward your best life.<br />But that's not all! As a subscriber, you'll receive an exclusive offer to book your first 60-minute psychological vitality consultation with me at 50% off the regular price. This session will help you identify your goals and overcome the barriers holding you back from a more fulfilling life.<br />And don't worry- you can unsubscribe at any time.</p>
+      </div>
 
       <div className="retake-div">
         <button
@@ -31,7 +41,7 @@ function ResultsScreen(props: ResultsScreenProps) {
           onClick={props.retake}
         >Retake Quiz</button>
       </div>
-    </>
+    </div>
   )
 }
 
